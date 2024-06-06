beaff34614aa9e6215c1bbeeb7fc75d7f7e7c70d
laradrb <laradarriba@hotmail.com>
2024-06-06 11:09:01 +0200

    completar css y js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const jokeEl = document.('')
-const jokeBtn = document.('')
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
 
-jokeBtn.addEventListener('', generateJoke)
+jokeBtn.addEventListener('click', generateJoke);
 
 // USING ASYNC/AWAIT
 async function generateJoke() {
   const config = {
     headers: {
-      Accept: '',
+      Accept: 'application/json',
     },
   }
 
-  const res = await ('', config)
+  const res = await fetch('https://icanhazdadjoke.com', config);
 
-  const data = await res.json()
+  const data = await res.json();
 
-  jokeEl.innerHTML = data.joke
+  jokeEl.innerHTML = data.joke;
 }
 
 //   HINTS
